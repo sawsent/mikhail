2245fd899411f716d7712241d9c2d645121f4b23
Vicente Figueiredo <vicenteppfigueiredo@gmail.com>
2024-10-29 19:07:25 +0000

    mikhail setup works as expected on windows.

--- src/mikhail.bat
+++ src/mikhail.bat
@@ -1,7 +1,9 @@
 @echo off
 
-call %~dp0..\.venv\Scripts\activate
-
-python %~dp0mikhail.py %*
-
-deactivate
+if "%~1" == "setup" (
+    python %~dp0mikhail.py %*
+) else (
+    call %~dp0..\.venv\Scripts\activate > nul 2>&1
+    python %~dp0mikhail.py %*
+    deactivate > nul 2>&1 
+)
--- src/mikhail.sh
+++ src/mikhail.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 MIKHAIL_LOCATION="$HOME/mikhail"
+
 exec > /dev/null 2>&1
 source $MIKHAIL_LOCATION/.venv/bin/activate
 exec > /dev/tty 2>&1
+
 python $MIKHAIL_LOCATION/src/mikhail.py $@ -macos
+
 exec > /dev/null 2>&1
 deactivate
 stty sane
